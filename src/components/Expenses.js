10b715989986caa34afd0dcd903cfe5362234259
RomuloDurante <romulodurante@protonmail.com>
2023-03-07 15:12:01 -0300

    card

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css"
+
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.dataBase.map((expense) => (
         <ExpenseItem
           title={expense.title}
@@ -10,7 +12,7 @@ const Expenses = (props) => {
         ></ExpenseItem>
       ))}
       
-    </div>
+    </Card>
   );
 };
 
